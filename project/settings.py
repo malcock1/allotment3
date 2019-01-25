7e124c4a4e63d6bfdb18853fd55d4c98ab4e244b
Martin Alcock <martin.alcock87@gmail.com>
2019-01-25 15:25:03 +0000

    Missed some apps in settingd.py

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'django_common',
+    'bootstrap4',
     'welcome',
     'registration',
     'dashboard',

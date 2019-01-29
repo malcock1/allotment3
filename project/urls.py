f21b7acb642cd381b87296639fa6f3a3c5a857a5
Martin Alcock <martin.alcock87@gmail.com>
2019-01-29 12:30:42 +0000

    Removed welcome app

--- project/urls.py
+++ project/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
+from health_check import health
+
 import plants, planner, designs
 
 from registration.views import exit_page, signup
 from dashboard.views import dashboard
-
 
 urlpatterns = [
     # Examples:
@@ -15,14 +15,13 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', index),
-    url(r'^health$', health),
     url(r'^admin/', admin.site.urls),
 
 
     url(r'^', include('django.contrib.auth.urls')),
-    # url(r'^bye/$', exit_page, name='exit_page'),
+    url(r'^/$', dashboard, name='home'),
+    url(r'^health$', health),
     url(r'^sign-up/$', signup, name='signup'),
-    url(r'^dashboard/$', dashboard, name='home'),
     url(r'^plants/', include('plants.urls')),
     url(r'^planner/', include('planner.urls')),
     url(r'^designs/', include('designs.urls')),
